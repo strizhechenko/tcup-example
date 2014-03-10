970113c6cce3228bff584fa46439d4324e0a7d41
Oleg Strizhechenko <oleg@carbonsoft.ru>
2014-03-10 21:18:17 +0500

    Create tcup.sh

---
+++ tcup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+. /etc/init.d/functions
+
+
+declare -i call_count=0
+declare -a results
+declare -a outputs
+
+_echo() {
+        echo -n $@
+}
+
+echo_result() {
+        case $1 in
+                0 )
+                        echo_success
+                        ;;
+                255 )
+                        echo_passed
+                        ;;
+                * )
+                        echo_failure
+                        ;;
+        esac
+        echo
+}
+
+run() {
+        output="$($@)"
+        local result=$?
+        results=( ${results[@]} $result )
+        if [ -z "$output" ]; then
+                output="-"
+        fi
+        outputs=( ${outputs[@]} "$output" )
+        ((call_count++))
+        echo $@
+}
+
+print_result() {
+        row=0
+        for i in ${results[@]}; do
+                tput cup $row 30
+                echo ${outputs[$row]}
+                tput cup $row 60
+                echo_result $i
+                sleep $((RANDOM%2))
+                ((row++))
+        done
+}
+
+main() {
+        clear
+        run echo one
+        run echo two
+        run echo three
+        run false
+        run echo 123
+        run false
+        run echo 345
+        print_result
+}
+
+main $@
